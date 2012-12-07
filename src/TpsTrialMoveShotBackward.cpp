3c4acc9a777e5a462cdae63f74470afb93e31e3b
Aaron Keys <askeys@silver.cchem.berkeley.edu>
2012-12-07 00:02:26 -0800

    updated forward and backward shot ctors

--- src/TpsTrialMoveShotBackward.cpp
+++ src/TpsTrialMoveShotBackward.cpp
@@ -1,44 +1,11 @@
 /*
-LibTPS -- A Transition Path Sampling Library
-(LibTPS) Open Source Software License Copyright 2012 The Regents of the 
-University of Michigan All rights reserved.
-
-LibTPS may contain modifications ("Contributions") provided, and to which
-copyright is held, by various Contributors who have granted The Regents of the
-University of Michigan the right to modify and/or distribute such Contributions.
-
-You may redistribute, use, and create derivate works of LibTPS, in source
-and binary forms, provided you abide by the following conditions:
-
-* Redistributions of source code must retain the above copyright notice, this
-list of conditions, and the following disclaimer both in the code and
-prominently in any materials provided with the distribution.
-
-* Redistributions in binary form must reproduce the above copyright notice, this
-list of conditions, and the following disclaimer in the documentation and/or
-other materials provided with the distribution.
-
-* Apart from the above required attributions, neither the name of the copyright
-holder nor the names of LibTPS's contributors may be used to endorse or
-promote products derived from this software without specific prior written
-permission.
-
-Disclaimer
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS ``AS IS'' AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND/OR ANY
-WARRANTIES THAT THIS SOFTWARE IS FREE OF INFRINGEMENT ARE DISCLAIMED.
-
-IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
-INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
+ *  TpsTrialMoveShotBackward.cpp
+ *  tpsapi
+ *
+ *  Created by askeys on 10/19/08.
+ *  Copyright 2008 __MyCompanyName__. All rights reserved.
+ *
+ */
 
 #include "TpsTrialMoveShotBackward.h"
 #include "TpsAlgorithmTPS.h"
@@ -48,7 +15,8 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "TpsTimeslice.h"
 #include <iostream>
 
-TpsTrialMoveShotBackward::TpsTrialMoveShotBackward()
+TpsTrialMoveShotBackward::TpsTrialMoveShotBackward(int max_steps)
+    : _max_steps(max_steps)
 {
 	
 }
@@ -67,12 +35,19 @@ bool TpsTrialMoveShotBackward::doMove(TpsAlgorithmTPS& controller)
 	TpsRNG& rng = controller.getRNG();
 	TpsOrderParameter& path_function = controller.getPathFunction();
 	
-	new_trajectory.clear();	
-	new_trajectory.shootFrom(old_trajectory, rng);	
-	int restart_time = new_trajectory.getTimeslice(0).getTimesteps();	
-	new_trajectory.copy(old_trajectory, restart_time, INT_MAX);
-	
+    int restart_time = 0;
+    while (1) {
+        new_trajectory.clear();
+        new_trajectory.shootFrom(old_trajectory, rng);
+        restart_time = new_trajectory.getTimeslice(0).getTimesteps();
+        int first_time = old_trajectory.getTimeslice(0).getTimesteps();
+        if (restart_time - first_time <= _max_steps) {
+            break;
+        }
+    }
+	new_trajectory.copy(old_trajectory, restart_time, INT_MAX);	
 	new_trajectory.extendBackward();
+
 	if(path_function.hA(new_trajectory) && path_function.hBorHB(new_trajectory)) {
 		controller.getTrajectoryFactory().moveTrajectory(-1, num+1);
 		controller.incrementPathNumber();
--- src/TpsTrialMoveShotForward.h
+++ src/TpsTrialMoveShotForward.h
@@ -1,49 +1,9 @@
 /*
-LibTPS -- A Transition Path Sampling Library
-(LibTPS) Open Source Software License Copyright 2012 The Regents of the 
-University of Michigan All rights reserved.
-
-LibTPS may contain modifications ("Contributions") provided, and to which
-copyright is held, by various Contributors who have granted The Regents of the
-University of Michigan the right to modify and/or distribute such Contributions.
-
-You may redistribute, use, and create derivate works of LibTPS, in source
-and binary forms, provided you abide by the following conditions:
-
-* Redistributions of source code must retain the above copyright notice, this
-list of conditions, and the following disclaimer both in the code and
-prominently in any materials provided with the distribution.
-
-* Redistributions in binary form must reproduce the above copyright notice, this
-list of conditions, and the following disclaimer in the documentation and/or
-other materials provided with the distribution.
-
-* Apart from the above required attributions, neither the name of the copyright
-holder nor the names of LibTPS's contributors may be used to endorse or
-promote products derived from this software without specific prior written
-permission.
-
-Disclaimer
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDER AND CONTRIBUTORS ``AS IS'' AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, AND/OR ANY
-WARRANTIES THAT THIS SOFTWARE IS FREE OF INFRINGEMENT ARE DISCLAIMED.
-
-IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
-INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
-OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
-ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-/*
  *  TpsTrialMoveShotForward.h
  *  tpsapi
  *
  *  Created by askeys on 10/19/08.
- *  Copyright 2008 University of Michigan. All rights reserved.
+ *  Copyright 2008 __MyCompanyName__. All rights reserved.
  *
  */
 
@@ -51,6 +11,7 @@ ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #define TPSTRIALMOVEFORWARDSHOT
 
 #include "TpsTrialMoveShot.h"
+#include <limits.h>
 
 /**
 \brief Performs a forward shooting move
@@ -67,11 +28,12 @@ with deterministic dynamics.
 class TpsTrialMoveShotForward : public TpsTrialMoveShot
 {
 	public:
-		TpsTrialMoveShotForward();
+		TpsTrialMoveShotForward(int=INT_MAX);
 		virtual ~TpsTrialMoveShotForward();
 		
 	protected:
 		virtual bool doMove(TpsAlgorithmTPS&);
+        int _max_steps;
 };
 
 #endif
